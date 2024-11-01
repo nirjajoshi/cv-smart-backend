0f27bd1587e9b4c48ee70d46e8fb600531e15df4
nirjajoshi <nirjajoshi01@gmail.com>
2024-11-01 17:02:36 +0530

    first commit

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,14 +1,21 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
-import {loginUser} from"../controllers/login_user.controller.js";
+import {loginUser} from "../controllers/login_user.controller.js";
 import { logoutUser } from "../controllers/logout_user.controller.js"; 
 import {updateUserStatus} from"../controllers/update_status.controller.js";
-import {saveUserUpdates} from"../controllers/save_status.controller.js";
+import {saveUserUpdates} from "../controllers/save_status.controller.js";
 
 const router = Router()
 
 router.route("/register").post(registerUser)
-router.route("/login").post(loginUser)
+router.route("/login").post(async (req, res) => {
+    try {
+        await loginUser(req, res);
+    } catch (error) {
+        console.error("Login error:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
 router.route("/logout").post(logoutUser); 
 router.route("/update-status").post(updateUserStatus); 
 router.route("/save-status").post(saveUserUpdates);
