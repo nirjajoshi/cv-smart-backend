9a662e2270d4a2154a822ecbcf4887fc5bf5c807
nirjajoshi <nirjajoshi01@gmail.com>
2024-11-01 17:25:00 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -2,21 +2,31 @@
 
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-import app from "./app.js"
+import app from "../api/index.js"
 dotenv.config({
     path: './env'
 })
 
 connectDB()
-
-.then(()=>{
-    app.listen(process.env.PORT || 8000,'0.0.0.0' , () =>{
-        console.log(`Server is running at port:${process.env.PORT || 8000}`);
+    .then(() => {
+        console.log("MongoDB connected successfully.");
     })
-})
-.catch((err) =>{
-    console.log("MongoDB connection failed ",err);
-})
+    .catch((err) => {
+        console.log("MongoDB connection failed", err);
+    });
+
+export default app;    
+
+// connectDB()
+
+// .then(()=>{
+//     app.listen(process.env.PORT || 8000,'0.0.0.0' , () =>{
+//         console.log(`Server is running at port:${process.env.PORT || 8000}`);
+//     })
+// })
+// .catch((err) =>{
+//     console.log("MongoDB connection failed ",err);
+// })
 
 
 // Use this code or just go to db folder index.js use that .This gives a systematic look to main index.js becuase very few code is written here 
