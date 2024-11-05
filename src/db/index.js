cf746a9985aca472b6b5eb4be9e1ac5f3c86d2e6
nirjajoshi <nirjajoshi01@gmail.com>
2024-11-05 10:32:51 +0530

    first index change

--- src/db/index.js
+++ src/db/index.js
@@ -1,21 +1,16 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js"; 
 
-
-const connectDB = async() =>{
+const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`,{
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
         
-        console.log(`MongoDB connected , DB Host :${connectionInstance.connection.host}`);
-        
+        console.log(`MongoDB connected, DB Host: ${connectionInstance.connection.host}`);
         
     } catch (error) {
-        console.error(" MONGODB connection error",error);
-        process.exit(1)  
+        console.error("MONGODB connection error", error);
+        process.exit(1);  
     }
 }
 
-export default connectDB 
+export default connectDB;
